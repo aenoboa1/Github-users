65261dbc7bbdf3c4956998078b7c75a16b256bfb
Andres Noboa V <56968012+aenoboa1@users.noreply.github.com>
2022-09-26 21:12:34 -0500

    header

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+            <h1> Hello from navbar  </h1>
+            </div>
+        );
+    }
+
+}
+
+
+
+
+
+export default Navbar ;
